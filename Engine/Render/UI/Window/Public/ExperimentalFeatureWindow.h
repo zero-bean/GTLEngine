ccfaeb3f5703449111d23f125a80411e966c7af4
TraceofLight <rickyjun96@gmail.com>
2025-09-10 18:45:43 +0900

    feat: input information widget
    Input Information Widget 추가
    - 해당 기능 Experimental로 이관
    - window에 사용하지 않는 내용 제거

--- Engine/Render/UI/Window/Public/ExperimentalFeatureWindow.h
+++ Engine/Render/UI/Window/Public/ExperimentalFeatureWindow.h
@@ -2,32 +2,12 @@
 #include "UIWindow.h"
 
 /**
- * @brief
- * 기존에 있던 Key Input Status를 분리한 클래스
+ * @brief 현재 단계에서 필요하지 않은 기능들을 모아둔 Window
+ * Key Input, Actor Termination Widget을 포함한다
  */
 class UExperimentalFeatureWindow : public UUIWindow
 {
 public:
 	UExperimentalFeatureWindow();
 	void Initialize() override;
-	// void Render() override;
-	// void Update() override;
-
-private:
-	// 키 입력 히스토리
-	TArray<FString> RecentKeyPresses;
-
-	// 마우스 관련
-	FVector LastMousePosition;
-	FVector MouseDelta;
-
-	// 키 입력 통계
-	TMap<FString, int> KeyPressCount;
-
-	void AddKeyToHistory(const FString& InKeyName);
-	static void RenderKeyList(const TArray<EKeyInput>& InPressedKeys);
-	void RenderMouseInfo() const;
-	void RenderKeyStatistics();
-
-	static void RenderControlHelp();
 };
