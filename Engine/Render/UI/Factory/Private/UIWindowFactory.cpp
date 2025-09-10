0fd6175af81640cdb1508bfdabaa785240b9ace1
TraceofLight <rickyjun96@gmail.com>
2025-09-10 16:06:40 +0900

    feat: actor transform
    Actor Transform UI 추가
    - TargetActorTransformWIdget 추가
    - 미사용 UI Outliner Window로 변경
    - Widget Postprocess 함수 추가로 후처리 구현
    - Factory 코드 수정

--- Engine/Render/UI/Factory/Private/UIWindowFactory.cpp
+++ Engine/Render/UI/Factory/Private/UIWindowFactory.cpp
@@ -6,7 +6,7 @@
 #include "Render/UI/Window/Public/ControlPanelWindow.h"
 #include "Render/UI/Window/Public/InputStatusWindow.h"
 #include "Render/UI/Window/Public/PerformanceWindow.h"
-#include "Render/UI/Window/Public/LevelManangerWindow.h"
+#include "Render/UI/Window/Public/OutlinerWindow.h"
 #include "Render/UI/Window/Public/CameraPanelWindow.h"
 
 UConsoleWindow* UUIWindowFactory::CreateConsoleWindow(EUIDockDirection InDockDirection)
@@ -14,6 +14,20 @@ UConsoleWindow* UUIWindowFactory::CreateConsoleWindow(EUIDockDirection InDockDir
 	auto& Window = UConsoleWindow::GetInstance();
 	Window.GetMutableConfig().DockDirection = InDockDirection;
 	return &Window;
+}
+
+UControlPanelWindow* UUIWindowFactory::CreateControlPanelWindow(EUIDockDirection InDockDirection)
+{
+	auto* Window = new UControlPanelWindow();
+	Window->GetMutableConfig().DockDirection = InDockDirection;
+	return Window;
+}
+
+UOutlinerWindow* UUIWindowFactory::CreateOutlinerWindow(EUIDockDirection InDockDirection)
+{
+	auto* Window = new UOutlinerWindow();
+	Window->GetMutableConfig().DockDirection = InDockDirection;
+	return Window;
 }
 
 UPerformanceWindow* UUIWindowFactory::CreatePerformanceWindow(EUIDockDirection InDockDirection)
@@ -26,20 +40,6 @@ UPerformanceWindow* UUIWindowFactory::CreatePerformanceWindow(EUIDockDirection I
 UInputStatusWindow* UUIWindowFactory::CreateInputStatusWindow(EUIDockDirection InDockDirection)
 {
 	auto* Window = new UInputStatusWindow();
-	Window->GetMutableConfig().DockDirection = InDockDirection;
-	return Window;
-}
-
-UControlPanelWindow* UUIWindowFactory::CreateActorInspectorWindow(EUIDockDirection InDockDirection)
-{
-	auto* Window = new UControlPanelWindow();
-	Window->GetMutableConfig().DockDirection = InDockDirection;
-	return Window;
-}
-
-ULevelManagerWindow* UUIWindowFactory::CreateLevelIOWindow(EUIDockDirection InDockDirection)
-{
-	auto* Window = new ULevelManagerWindow();
 	Window->GetMutableConfig().DockDirection = InDockDirection;
 	return Window;
 }
@@ -57,10 +57,10 @@ void UUIWindowFactory::CreateDefaultUILayout()
 
 	// 기본 레이아웃 생성
 	UIManager.RegisterUIWindow(CreateConsoleWindow(EUIDockDirection::Bottom));
-	UIManager.RegisterUIWindow(CreatePerformanceWindow(EUIDockDirection::Right));
-	UIManager.RegisterUIWindow(CreateInputStatusWindow(EUIDockDirection::Right));
-	UIManager.RegisterUIWindow(CreateActorInspectorWindow(EUIDockDirection::Left));
-	UIManager.RegisterUIWindow(CreateLevelIOWindow(EUIDockDirection::Center));
-	UIManager.RegisterUIWindow(CreateCameraPanelWindow(EUIDockDirection::Left));
+	UIManager.RegisterUIWindow(CreateControlPanelWindow(EUIDockDirection::Left));
+	UIManager.RegisterUIWindow(CreateOutlinerWindow(EUIDockDirection::Center));
+	// UIManager.RegisterUIWindow(CreatePerformanceWindow(EUIDockDirection::Right));
+	// UIManager.RegisterUIWindow(CreateInputStatusWindow(EUIDockDirection::Right));
+	// UIManager.RegisterUIWindow(CreateCameraPanelWindow(EUIDockDirection::Left));
 	UE_LOG("UIWindowFactory: Default UI Layout Created Successfully");
 }
--- Engine/Render/UI/Window/Public/ControlPanelWindow.h
+++ Engine/Render/UI/Window/Public/ControlPanelWindow.h
@@ -6,99 +6,12 @@ class ULevelManager;
 class AActor;
 
 /**
-* @brief Actor 속성을 편집하는 UI 윈도우
- * ImGuiManager의 Actor 편집 기능을 분리한 클래스
+ * @brief 전반적인 컨트롤을 담당하는 Panel Window
+ * FPS Status, Actor Spawn, Level IO, Camera Control 기능이 포함되어 있다
  */
 class UControlPanelWindow : public UUIWindow
 {
 public:
 	UControlPanelWindow();
 	void Initialize() override;
-
-	// Common Options
-	void RenderPrimitiveSpawn();
-	void RenderLevelIO();
-	void RenderCamera();
-
-	// FPS UI
-	static ImVec4 GetFPSColor(float InFPS);
-
-	// Level IO
-	path OpenSaveFileDialog();
-	path OpenLoadFileDialog();
-	void SaveLevel(const FString& InFilePath);
-	void LoadLevel(const FString& InFilePath);
-	void CreateNewLevel();
-
-	// Camera
-	void SyncFromCamera();
-	void PushToCamera();
-
-	AActor* GetSelectedActor() const { return SelectedActor; }
-	void SetSelectedActor(AActor* Actor) { SelectedActor = Actor; }
-
-private:
-
-
-	// Level IO
-	ULevelManager* LevelManager;
-	char NewLevelNameBuffer[256] = "Default";
-	FString StatusMessage;
-	float StatusMessageTimer = 0.0f;
-
-	static constexpr float STATUS_MESSAGE_DURATION = 3.0f;
-
-	// Camera
-	UCamera* CameraPtr = nullptr;
-	float UiFovY = 80.f;
-	float UiNearZ = 0.1f;
-	float UiFarZ = 1000.f;
-	int   CameraModeIndex = 0;
-
-	AActor* SelectedActor = nullptr;
-
-	// Transform 편집
-	FVector EditLocation;
-	FVector EditRotation;
-	FVector EditScale;
-
-	// UI 상태
-	bool bTransformChanged = false;
-
-	//Level Memory Property
-	int32 LevelObjectCount = 0;
-	int32 LevelObjectMemory = 0;
-
-	// Actor Spawn Property
-	int32 SelectedPrimitiveType = 0; // 0: Cube, 1: Sphere, 2: Triangle
-	int32 NumberOfSpawn = 1;
-	float SpawnRangeMin = -5.0f;
-	float SpawnRangeMax = 5.0f;
-
-	// 편집 모드
-	enum class EEditMode
-	{
-		Transform, // 트랜스폼 편집
-		Components, // 컴포넌트 편집
-		Properties // 속성 편집
-	};
-
-	EEditMode CurrentEditMode = EEditMode::Transform;
-
-	void RenderTransformEditor();
-	void RenderComponentEditor();
-	void RenderPropertyEditor();
-	void RenderActorInspection();
-	void RenderModeSelector();
-
-	// Actor Spawn & Delete
-	void RenderLevelMemorySection();
-	void RenderActorSpawnSection();
-	void RenderActorDeleteSection();
-	void SpawnActors() const;
-	void DeleteSelectedActor();
-
-	void UpdateTransformFromActor();
-	void ApplyTransformToActor();
-	bool HasValidActor() const { return SelectedActor != nullptr; }
 };
