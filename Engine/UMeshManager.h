c848e3b02867d7ce89196ec151a42b315103813a
sb-111 <117798778+sb-111@users.noreply.github.com>
2025-09-14 22:36:44 +0900

    UMesh 및 UMeshManager 수정
    FVertexPosTexCoord 정점 구조체도 받을 수 있게 변경

--- Engine/UMeshManager.h
+++ Engine/UMeshManager.h
@@ -20,6 +20,12 @@ private:
 	*/
 	UMesh* CreateMeshInternal(const TArray<FVertexPosColor>& vertices, const TArray<uint32>& indices,
 		D3D_PRIMITIVE_TOPOLOGY primitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	
+	/* *
+	* @brief - Vertex(Pos, UV) 및 Index 정보를 가지고 있는 Mesh를 생성할 때 사용합니다.
+	*/
+	UMesh* CreateMeshInternal(const TArray<FVertexPosTexCoord>& vertices, const TArray<uint32>& indices,
+		D3D_PRIMITIVE_TOPOLOGY primitiveType = D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
 public:
 	UMeshManager();
 	~UMeshManager();
