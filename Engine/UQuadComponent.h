29c1efda98ce401608fdbfd33f9b04c845a5b426
sb-111 <117798778+sb-111@users.noreply.github.com>
2025-09-14 22:34:22 +0900

    텍스처링 위한 쿼드 컴포넌트 추가

---
+++ Engine/UQuadComponent.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "URenderer.h"
+#include "UPrimitiveComponent.h"
+#include "FVertexPosColor.h"
+#include "Vector.h"
+class UQuadComponent : public UPrimitiveComponent
+{
+	DECLARE_UCLASS(UQuadComponent, UPrimitiveComponent)
+private:
+	bool IsManageable() override { return true; }
+
+public:
+	UQuadComponent(FVector pos = { 0, 0, 0 }, FVector rot = { 0, 0, 0 }, FVector scl = { 1, 1, 1 })
+		:UPrimitiveComponent(pos, rot, scl)
+	{
+	}
+};
