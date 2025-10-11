f9766f7e12279f8f7ec475fc4b37b35fe27bde1f
Soyeon Joo <sildredel@naver.com>
2025-10-12 01:45:46 +0900

    add: spotlight actor

---
+++ Engine/Source/Actor/Private/SpotLightActor.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "Actor/Public/SpotLightActor.h"
+
+#include "Component/Public/SpotLightComponent.h"
+#include "Component/Public/BillboardComponent.h"
+
+IMPLEMENT_CLASS(ASpotLightActor, ALightActor)
+
+ASpotLightActor::ASpotLightActor()
+{
+	auto SpotLightComponent = CreateDefaultSubobject<USpotLightComponent>("SpotLightComponent");
+	SetRootComponent(SpotLightComponent);
+	auto BillboardComponent = CreateDefaultSubobject<UBillboardComponent>("BillboardComponent");
+	BillboardComponent->SetSprite(ELightType::Spotlight);
+	SetRootComponent(BillboardComponent);
+}
