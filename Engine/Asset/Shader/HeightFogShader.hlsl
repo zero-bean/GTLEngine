011dc7fd22ec666024dbcb4878fcc2afbf78fd31
lorevoon <zoejsy22@gmail.com>
2025-10-16 04:11:05 +0900

    fix: fog calculation

--- Engine/Asset/Shader/HeightFogShader.hlsl
+++ Engine/Asset/Shader/HeightFogShader.hlsl
@@ -132,7 +132,7 @@ float4 mainPS(PS_INPUT input) : SV_TARGET
 	// FogHeightFalloff : 안개 고도 감쇠 팩터, 클수록 급격하게 옅어진다.
 	// FogDensity : 안개 밀도(0~1)
 	float sigma  = FogDensity * exp(-FogHeightFalloff * (zClosest - FogHeight));
-	const float UnitToMeters = 1.0f;
+	const float UnitToMeters = 10.0f;
 	// float opticalDepth = min(sigma * (Dist * UnitToMeters), 60.0f);
 	float fogFactor = exp(-sigma * (Dist * UnitToMeters));
 
