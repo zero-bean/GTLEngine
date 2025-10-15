aff58345fcfc3358890f1ee6c9e02ec40c29ae58
Soyeon Joo <sildredel@naver.com>
2025-10-15 16:03:57 +0900

    add: heightfog

---
+++ Engine/Source/Component/Public/HeightFogComponent.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "PrimitiveComponent.h"
+
+UCLASS()
+class UHeightFogComponent :  public UPrimitiveComponent
+{
+	GENERATED_BODY()
+	DECLARE_CLASS(UHeightFogComponent, UPrimitiveComponent)
+
+public:
+	UHeightFogComponent();
+	virtual ~UHeightFogComponent();
+
+	// UObject* Duplicate(FObjectDuplicationParameters Parameters) override;
+	// void Serialize(const bool bInIsLoading, JSON& InOutHandle) override;
+private:
+
+};
