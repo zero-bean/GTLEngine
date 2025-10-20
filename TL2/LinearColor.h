1f4c64bbf4a582e49e9d164c13892310b7f02f8d
WH <121390020+Jeongwoohyeong@users.noreply.github.com>
2025-10-20 21:31:09 +0900

    fix : uv to xy 공식 수정, XYZ to RGB 함수 추가

--- TL2/LinearColor.h
+++ TL2/LinearColor.h
@@ -1,9 +1,9 @@
 ﻿#pragma once
 
 inline FMatrix XYZtoRGB = {
-	3.2406f, -1.5372f, -0.4986f, 0.0f,
-	-0.9689f, 1.8758f, 0.0415f, 0.0f,
-	0.0557f, -0.2040f, 1.0570f, 0.0f,
+	3.2404542f, -1.5371385f, -0.4985314f, 0.0f,
+	-0.9692660f, 1.8760108f, 0.0415560f, 0.0f,
+	0.0556434f, -0.2040259f, 1.0572252f, 0.0f,
 	0.0f, 0.0f, 0.0f, 1.0f
 };
 
@@ -14,6 +14,23 @@ struct FLinearColor
 	float B;
 	float A;
 };
+
+inline FVector4 ConvertXYZToRGB(const FVector4& V, FMatrix& Matrix)
+{
+	return FVector4(
+		// Result.X = dot(V, Row 0)
+		V.X * Matrix.M[0][0] + V.Y * Matrix.M[0][1] + V.Z * Matrix.M[0][2] + V.W * Matrix.M[0][3],
+        
+		// Result.Y = dot(V, Row 1)
+		V.X * Matrix.M[1][0] + V.Y * Matrix.M[1][1] + V.Z * Matrix.M[1][2] + V.W * Matrix.M[1][3],
+        
+		// Result.Z = dot(V, Row 2)
+		V.X * Matrix.M[2][0] + V.Y * Matrix.M[2][1] + V.Z * Matrix.M[2][2] + V.W * Matrix.M[2][3],
+        
+		// Result.W = dot(V, Row 3)
+		V.X * Matrix.M[3][0] + V.Y * Matrix.M[3][1] + V.Z * Matrix.M[3][2] + V.W * Matrix.M[3][3]
+	);
+}
 
 inline FLinearColor MakeFromColorTemperature(float InTemperature)
 {
@@ -28,7 +45,7 @@ inline FLinearColor MakeFromColorTemperature(float InTemperature)
 
 	// CIE(x, y) 색도 좌표
 	float x = (3.0f * U) / ((2.0f * U) - (8.0f * V) + 4);
-	float y = (2.0f * U) / ((2.0f * U) - (8.0f * V) + 4);
+	float y = (2.0f * V) / ((2.0f * U) - (8.0f * V) + 4);
 	float z = 1.0f - x - y;
 
 	// CIE XYZ 색공간
@@ -36,7 +53,8 @@ inline FLinearColor MakeFromColorTemperature(float InTemperature)
 	// 나눗셈 최적화용으로 미리 계산
 	float Invy = 1.0f / y;
 	FVector4 XYZColorSpace = FVector4(Invy * x, 1.0f, Invy * z, 0.0f);
-	FVector4 NewRGB = XYZtoRGB.TransformPosition(XYZColorSpace);    
+	FVector4 NewRGB = ConvertXYZToRGB(XYZColorSpace, XYZtoRGB);
+	
 	return FLinearColor(
 		std::max(0.0f, NewRGB.X),
 		std::max(0.0f, NewRGB.Y),
