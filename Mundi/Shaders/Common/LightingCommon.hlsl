55ffdc4d28ea824cc1ea69473e1cf77610d352b9
devouring123 <devouring123@gmail.com>
2025-10-26 22:55:41 +0900

    fix: bias 0.005 to 0.0001

--- Mundi/Shaders/Common/LightingCommon.hlsl
+++ Mundi/Shaders/Common/LightingCommon.hlsl
@@ -163,9 +163,9 @@ float SampleSpotLightShadowMap(uint shadowMapIndex, float4 lightSpacePos)
     float currentDepth = projCoords.z;
 
     // Bias to prevent shadow acne
-    float bias = 0.005f;
+    float bias = 0.00001f;
     currentDepth -= bias;
-
+    
     // Use comparison sampler for hardware PCF (Percentage Closer Filtering)
     float3 shadowSampleCoord = float3(shadowTexCoord, shadowMapIndex);
     float shadow = g_SpotLightShadowMaps.SampleCmpLevelZero(g_ShadowSampler, shadowSampleCoord, currentDepth);
@@ -202,7 +202,7 @@ float SampleDirectionalLightShadowMap(uint shadowMapIndex, float4 lightSpacePos)
     float currentDepth = projCoords.z;
 
     // Bias to prevent shadow acne (can be adjusted for directional lights)
-    float bias = 0.005f;
+    float bias = 0.0001f;
     currentDepth -= bias;
 
     // Use comparison sampler for hardware PCF (Percentage Closer Filtering)
